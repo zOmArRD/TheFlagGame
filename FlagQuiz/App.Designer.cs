d5636f68f277c04e6aef6eb8347bd1962657040a
zOmArRD <omarsena25@gmail.com>
2024-06-09 01:30:33 -0400

    Adding Files

---
+++ FlagQuiz/App.Designer.cs
@@ -0,0 +1,169 @@
+﻿/*
+ * Created by zOmArRD using Rider by JetBrains
+ *
+ * Date: 09/06/2004
+ */
+namespace FlagQuiz
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            flagBox = new PictureBox();
+            label1 = new Label();
+            comboBoxCountries = new ComboBox();
+            resultTextBox = new TextBox();
+            submitBtn = new Button();
+            scoreTextBox = new TextBox();
+            nextBtn = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)flagBox).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(flagBox);
+            groupBox1.Dock = DockStyle.Left;
+            groupBox1.Location = new Point(15, 15);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(10);
+            groupBox1.Size = new Size(313, 181);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Flag";
+            // 
+            // flagBox
+            // 
+            flagBox.Dock = DockStyle.Fill;
+            flagBox.Image = Properties.Resources.Dominican_Republic;
+            flagBox.Location = new Point(10, 26);
+            flagBox.Name = "flagBox";
+            flagBox.Size = new Size(293, 145);
+            flagBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            flagBox.TabIndex = 1;
+            flagBox.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.Location = new Point(367, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(114, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Select Country:";
+            // 
+            // comboBoxCountries
+            // 
+            comboBoxCountries.FormattingEnabled = true;
+            comboBoxCountries.Location = new Point(367, 73);
+            comboBoxCountries.Name = "comboBoxCountries";
+            comboBoxCountries.Size = new Size(154, 23);
+            comboBoxCountries.TabIndex = 2;
+            // 
+            // resultTextBox
+            // 
+            resultTextBox.Font = new Font("Segoe UI", 12F);
+            resultTextBox.Location = new Point(367, 114);
+            resultTextBox.Name = "resultTextBox";
+            resultTextBox.ReadOnly = true;
+            resultTextBox.Size = new Size(154, 29);
+            resultTextBox.TabIndex = 3;
+            resultTextBox.Text = "Correct!";
+            resultTextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // submitBtn
+            // 
+            submitBtn.Font = new Font("Segoe UI", 12F);
+            submitBtn.Location = new Point(562, 40);
+            submitBtn.Name = "submitBtn";
+            submitBtn.Size = new Size(131, 45);
+            submitBtn.TabIndex = 4;
+            submitBtn.Text = "Submit";
+            submitBtn.UseVisualStyleBackColor = true;
+            submitBtn.MouseClick += OnSubmit;
+            // 
+            // scoreTextBox
+            // 
+            scoreTextBox.Font = new Font("Segoe UI", 12F);
+            scoreTextBox.Location = new Point(367, 167);
+            scoreTextBox.Name = "scoreTextBox";
+            scoreTextBox.ReadOnly = true;
+            scoreTextBox.Size = new Size(326, 29);
+            scoreTextBox.TabIndex = 6;
+            scoreTextBox.Text = "Excelent";
+            scoreTextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // nextBtn
+            // 
+            nextBtn.Font = new Font("Segoe UI", 12F);
+            nextBtn.Location = new Point(562, 98);
+            nextBtn.Name = "nextBtn";
+            nextBtn.Size = new Size(131, 45);
+            nextBtn.TabIndex = 7;
+            nextBtn.Text = "Next";
+            nextBtn.UseVisualStyleBackColor = true;
+            nextBtn.MouseClick += OnNext;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(717, 211);
+            Controls.Add(nextBtn);
+            Controls.Add(scoreTextBox);
+            Controls.Add(submitBtn);
+            Controls.Add(resultTextBox);
+            Controls.Add(comboBoxCountries);
+            Controls.Add(label1);
+            Controls.Add(groupBox1);
+            MaximizeBox = false;
+            MaximumSize = new Size(733, 250);
+            MinimumSize = new Size(733, 250);
+            Name = "App";
+            Padding = new Padding(15);
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)flagBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private PictureBox flagBox;
+        private Label label1;
+        private ComboBox comboBoxCountries;
+        private TextBox resultTextBox;
+        private Button submitBtn;
+        private TextBox scoreTextBox;
+        private Button nextBtn;
+    }
+}
---
+++ FlagQuiz/App.cs
@@ -0,0 +1,150 @@
+﻿/*
+ * Created by zOmArRD using Rider by JetBrains
+ *
+ * Date: 09/06/2004
+ */
+
+using FlagQuiz.Properties;
+
+namespace FlagQuiz
+{
+    public partial class App : Form
+    {
+        private readonly List<string> _countries =
+        [
+            "Anguilla", "Antigua and Barbuda", "Argentina", "Aruba", "Bahamas", "Barbados", "Belize", "Bermuda",
+            "Bolivia", "Brazil", "Canada", "Caribbean Netherlands", "Cayman Islands", "Chile", "Colombia", "Costa Rica",
+            "Cuba", "Curacao", "Dominica", "Dominican Republic", "Ecuador", "Estados Unidos", "Guatemala", "Haiti",
+            "Honduras", "Jamaica", "Mexico", "Nicaragua", "Panama", "Paraguay", "Peru", "Puerto Rico", "Suriname",
+            "Uruguay", "Venezuela"
+        ];
+
+        private readonly List<Bitmap> _flags = [];
+        private int _currentFlagIndex;
+        private int _correctAnswers;
+        
+        public App()
+        {
+            InitializeComponent();
+            LoadFlags();
+            ShuffleFlags();
+            comboBoxCountries.DataSource = new BindingSource(_countries, null);
+            ShowNextFlag();
+        }
+
+        private void LoadFlags()
+        {
+            foreach (var country in _countries)
+            {
+                var resourceName = country.Replace(" ", "_");
+                var flag = (Bitmap)Resources.ResourceManager.GetObject(resourceName);
+
+                if (flag != null)
+                {
+                    _flags.Add(flag);
+                }
+                else
+                {
+                    MessageBox.Show($"Flag not found for {country}", "Error", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+        }
+        
+        private void ShuffleFlags()
+        {
+            Random rand = new();
+
+            for (var i = 0; i < _flags.Count; i++)
+            {
+                var j = rand.Next(i, _flags.Count);
+
+                (_flags[i], _flags[j]) = (_flags[j], _flags[i]);
+                (_countries[i], _countries[j]) = (_countries[j], _countries[i]);
+            }
+        }
+
+        private void ShowNextFlag()
+        {
+            if (_currentFlagIndex >= _flags.Count)
+            {
+                EvaluateResults();
+                return;
+            }
+
+            submitBtn.Enabled = true;
+            flagBox.Image = _flags[_currentFlagIndex];
+
+            List<string> tempCountries = [.._countries];
+            tempCountries.Shuffle();
+
+            comboBoxCountries.DataSource = new BindingSource(tempCountries, null);
+            comboBoxCountries.SelectedIndex = -1;
+
+            resultTextBox.Text = string.Empty;
+        }
+
+        private void OnSubmit(object sender, MouseEventArgs e)
+        {
+            if (comboBoxCountries.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a country", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var selectedCountry = comboBoxCountries.SelectedItem.ToString();
+            var correctCountry = _countries[_currentFlagIndex];
+
+            if (selectedCountry == correctCountry)
+            {
+                _correctAnswers++;
+                resultTextBox.Text = "Correct!";
+            }
+            else
+            {
+                resultTextBox.Text = "Incorrect!";
+            }
+
+            _currentFlagIndex++;
+            submitBtn.Enabled = false;
+        }
+
+        private void OnNext(object sender, MouseEventArgs e)
+        {
+            ShowNextFlag();
+        }
+
+        private void EvaluateResults()
+        {
+            submitBtn.Enabled = false;
+            nextBtn.Enabled = false;
+
+            var evaluation = _correctAnswers switch
+            {
+                5 => "fail",
+                15 => "Poor",
+                20 => "Good",
+                24 => "Very Good!",
+                _ => "Excellent!"
+            };
+
+            scoreTextBox.Text = $@"Final Score: {_correctAnswers} - {evaluation}";
+        }
+    }
+
+    public static class ListExtensions
+    {
+        public static void Shuffle<T>(this List<T> list)
+        {
+            Random rng = new();
+
+            var n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = rng.Next(n + 1);
+                (list[k], list[n]) = (list[n], list[k]);
+            }
+        }
+    }
+}
